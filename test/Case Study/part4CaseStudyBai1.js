e6a8a1021a9ac184885ad997b1932b31fa0cd92a
tienyasuo729 <tienngqueson@gmail.com>
2022-11-02 01:14:56 +0700

    codegym

--- test/Case Study/part4CaseStudyBai1.js
+++ test/Case Study/part4CaseStudyBai1.js
@@ -9,6 +9,7 @@ function part4CaseStudyBai1() {
     for (let i = 0; i < arr.length ; i++) {
         if (arr[i] === " " && arr[i+1] === " " ) {
             arr.splice(i, 1);
+            --i;
         }
     }
     for (let i = 0; i < arr.length; i++) {
