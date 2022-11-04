c2cdb7c7df7b4715c5dcfd9a5acd7a173284d327
tienyasuo729 <tienngqueson@gmail.com>
2022-11-04 21:01:57 +0700

    codegym

--- test/Case Study/part4CaseStudyBai2.js
+++ test/Case Study/part4CaseStudyBai2.js
@@ -1,4 +1,3 @@
-// tiến dẻ rách
 function part4CaseStudyBai2() {
     let strings = prompt("Nhập vào đây một chuỗi cần kiểm tra");
     let arr = Array();
@@ -7,44 +6,17 @@ function part4CaseStudyBai2() {
     }
     for (let i = 0; i < arr.length ; i++) {
         let check = 0;
-        for (let j = 0; j < i ; j++) {
-            if (arr[i] === arr[j]) {
+        for (let j = 0; j < arr.length ; j++) {
+            if (arr[i] === arr[j] && i !== j) {
                 check++;
-                arr.splice(j-1,1);
+                arr.splice(j,1);
                 j--;
             }
         }
         // if (check > 0) {
-        //     arr.splice(i-1,1);
+        //     arr.splice(i,1);
         //     i--;
         // }
     }
-    alert(arr);
+    alert("DifferentSymbolsNaise(s) :" + arr.length + ". Phần tử khác nhau là :" + arr);
 }
-
-// nam đb
-// function part4CaseStudyBai2() {
-//     let strings = prompt("Nhập vào đây một chuỗi cần kiểm tra");
-//     let arr = Array();
-//     // for (let i = 0; i < strings.length; i++) {
-//     //     arr[i] = strings[i];
-//     // }
-//     for (let i = 0; i < strings.length; i++) {
-//         // let check = 0;
-//         for (let j = 0; j < i; j++) {
-//             if (arr[i] === arr[j]) {
-//                 // check++;
-//                 // arr.splice(j - 1, 1);
-//                 // j--;
-//                 arr.push(arr[i]);
-//                 strings.splice(i, 1);
-//                 break;
-//             }
-//         }
-//         // if (check > 0) {
-//         //     arr.splice(i-1,1);
-//         //     i--;
-//         // }
-//     }
-//     alert(arr);
-// }
--- test/Case Study/part4CaseStudyBai3.js
+++ test/Case Study/part4CaseStudyBai3.js
@@ -1,3 +1,44 @@
 function part4CaseStudyBai3() {
-
+    let strings1 = prompt("Nhập xâu S1 :");
+    let strings2 = prompt("Nhập xâu S2 :");
+    // let arrs1 = ["a", "a", "b", "c", "c"]; // test
+    // let arrs2 = ["a", "d", "c", "a", "a"]; // test
+    let arrs1 = ["a", "a", "b", "c", "c"];
+    let arrs2 = ["a", "d", "c", "a", "a"];
+    let arr = Array();
+    let n = 0;
+    let commonCharacterCount = 0;
+    let count = 1;
+    for (let i = 0; i < strings1.length; i++) {
+        arrs1[i] = strings1[i];
+    }
+    for (let i = 0; i < strings2.length; i++) {
+        arrs2[i] = strings2[i];
+    }
+    for (let i = 0; i < arrs1.length; i++) {
+        for (let j = 0; j < arrs2.length; j++) {
+            if (arrs1[i] === arrs2[j]) {
+                ++commonCharacterCount;
+                arr[n] = arrs1[i];
+                n++;
+                arrs1.splice(i, 1);
+                arrs2.splice(j, 1);
+                --i;
+                --j;
+            }
+        }
+    }
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] === arr[j] && i !== j) {
+                ++count;
+                arr.splice(j, 1);
+                --j;
+            }
+        }
+        arr[i] = count + " ký tự " + arr[i];
+        count = 1;
+    }
+    alert(commonCharacterCount);
+    alert(arr);
 }
