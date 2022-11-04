3ae8f290992edbb075337e790a2ee5d64e2dcdc5
tienyasuo729 <tienngqueson@gmail.com>
2022-11-05 02:04:57 +0700

    codegym

--- test/Case Study/part4CaseStudyBai2.js
+++ test/Case Study/part4CaseStudyBai2.js
@@ -4,6 +4,25 @@ function part4CaseStudyBai2() {
     for (let i = 0; i < strings.length ; i++) {
         arr[i] = strings[i];
     }
+    // chỉ lấy phần tử không bị chùng
+    // for (let i = 0; i < arr.length ; i++) {
+    //     let check = 0;
+    //     for (let j = 0; j < arr.length ; j++) {
+    //         if (arr[i] === arr[j] && i !== j) {
+    //             ++check;
+    //             arr.splice(j, 1);
+    //             --j;
+    //         }
+    //     }
+    //     if (check > 0) {
+    //         arr.splice(i, 1);
+    //         check = 0;
+    //         --i;
+    //     }
+    // }
+    // alert(arr);
+
+    // làm theo đề
     for (let i = 0; i < arr.length ; i++) {
         let check = 0;
         for (let j = 0; j < arr.length ; j++) {
@@ -13,10 +32,6 @@ function part4CaseStudyBai2() {
                 j--;
             }
         }
-        // if (check > 0) {
-        //     arr.splice(i,1);
-        //     i--;
-        // }
     }
     alert("DifferentSymbolsNaise(s) :" + arr.length + ". Phần tử khác nhau là :" + arr);
 }
