fffed0179effe543806f7f694682b0fc00cd52f2
tienyasuo729 <tienngqueson@gmail.com>
2022-11-17 22:00:05 +0700

    codegym

--- java/java/src/test2.java
+++ java/java/src/test2.java
@@ -1,48 +1,50 @@
 import java.util.Scanner;
 
-public class test {
+public class test2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String print = "Đọc số cần in ra màn hình là : ";
         int number;
         do {
             System.out.print("Mời nhập số nguyên không âm dưới 1000 : ");
-           number = Integer.parseInt(sc.next());
+            number = Integer.parseInt(sc.next());
         } while (number >= 1000 || number < 0);
         if (number <= 10) {
             switch (number) {
                 case 1:
-                    System.out.println("Một");
+                    print += "Một";
                     break;
                 case 2:
-                    System.out.println("Hai");
+                    print +="Hai";
                     break;
                 case 3:
-                    System.out.println("Ba");
+                    print +="Ba";
                     break;
                 case 4:
-                    System.out.println("Bốn");
+                    print +="Bốn";
                     break;
                 case 5:
-                    System.out.println("Năm ");
+                    print +="Năm";
                     break;
                 case 6:
-                    System.out.println("Sáu");
+                    print +="Sáu";
                     break;
                 case 7:
-                    System.out.println("Bảy");
+                    print +="Bảy";
                     break;
                 case 8:
-                    System.out.println("Tám");
+                    print +="Tám";
                     break;
                 case 9:
-                    System.out.println("Chín");
+                    print +="Chín";
                     break;
                 case 10:
-                    System.out.println("Mười");
+                    print +="Mười";
                     break;
                 default:
-                    System.out.println("Không");
+                    print +="Không";
             }
+            System.out.println(print);
         }else if (number < 100) {
             System.out.println("2 số");
         }else {
@@ -50,4 +52,5 @@ public class test {
         }
         System.out.print("Số vừa nhập là : " + number);
     }
+
 }
