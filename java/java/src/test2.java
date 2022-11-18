cf888bbcd9e0a232d12c75495987ff38ac1e1592
tienyasuo729 <tienngqueson@gmail.com>
2022-11-18 19:37:12 +0700

    codegym

--- java/java/src/test2.java
+++ java/java/src/test2.java
@@ -1,59 +1,76 @@
-import java.security.PrivateKey;
 import java.util.Scanner;
 
 public class test2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number;
+        String prin = "";
         do {
             System.out.print("Mời nhập số nguyên không âm dưới 1000 : ");
             number = Integer.parseInt(sc.next());
         } while (number >= 1000 || number < 0);
         if (number <= 10) {
-            System.out.println(bbb(number));
-        }else if (number < 100) {
-            System.out.println("2 số");
-        }else {
-            System.out.println("3 số");
+            prin = read(number);
+            System.out.println("Đọc số cần in ra màn hình là : " + prin);
+        } else if (number % 10 == 0 && number < 100) {
+            int ahi = number / 10;
+            prin = read(ahi) + " mươi";
+            System.out.println("Đọc số cần in ra màn hình là : " + prin);
+        } else if (number % 100 == 0 && number >= 100) {
+            int ahihi = number / 100;
+            prin = read(ahihi) + " trăm";
+            System.out.println("Đọc số cần in ra màn hình là : " + prin);
+        } else if (number < 100) {
+            int firstNumber = number / 10;
+            int secondNumber = number % 10;
+            prin += read(firstNumber) + " mươi " + read(secondNumber);
+            System.out.println("Đọc số cần in ra màn hình là : " + prin);
+        } else {
+            int firstNumber = number / 100;
+            int secondNumber = (number - (firstNumber * 100)) / 10;
+            int thirdNumber = number % 10;
+            prin += read(firstNumber) + " trăm " + read(secondNumber) + " mươi " + read(thirdNumber);
+            System.out.println("Đọc số cần in ra màn hình là : " + prin);
         }
         System.out.print("Số vừa nhập là : " + number);
     }
-    private static String bbb(int number){
-        String print = "Đọc số cần in ra màn hình là : ";
+
+    private static String read(int number) {
+        String print = "";
         switch (number) {
             case 1:
                 print += "Một";
                 break;
             case 2:
-                print +="Hai";
+                print += "Hai";
                 break;
             case 3:
-                print +="Ba";
+                print += "Ba";
                 break;
             case 4:
-                print +="Bốn";
+                print += "Bốn";
                 break;
             case 5:
-                print +="Năm";
+                print += "Năm";
                 break;
             case 6:
-                print +="Sáu";
+                print += "Sáu";
                 break;
             case 7:
-                print +="Bảy";
+                print += "Bảy";
                 break;
             case 8:
-                print +="Tám";
+                print += "Tám";
                 break;
             case 9:
-                print +="Chín";
+                print += "Chín";
                 break;
             case 10:
-                print +="Mười";
+                print += "Mười";
                 break;
             default:
-                print +="Không";
+                print += "Không";
         }
+        return (print);
     }
-    return String;
 }
