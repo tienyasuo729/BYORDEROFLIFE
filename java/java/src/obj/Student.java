9e03587c4c471cdec7f23c60e730b44ce3e6049d
tienyasuo729 <tienngqueson@gmail.com>
2022-11-23 19:36:19 +0700

    codegym

--- java/java/src/obj/Student.java
+++ java/java/src/obj/Student.java
@@ -3,5 +3,10 @@ package obj;
 public class Student {
     int x;
     String name;
+    public Student(int x, String name) {
+        this.x = x;
+        this.name = name;
+    }
 }
+
 
--- java/java/src/obj/demoObj.java
+++
@@ -1,8 +0,0 @@
-package obj;
-
-public class demoObj {
-    public static void main(String[] args) {
-        Student student1 = new Student(1,"tiến");
-        System.out.print(student1.name);
-    }
-}
