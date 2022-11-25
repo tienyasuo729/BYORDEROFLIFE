b47b42b768f6799e1ab2afc8086d124ecf090884
tienyasuo729 <tienngqueson@gmail.com>
2022-11-25 16:52:31 +0700

    codegym

--- java/java/src/demoOBJ/demo.java
+++ java/java/src/demoOBJ/demo.java
@@ -1,39 +1,5 @@
 package demoOBJ;
 
 public class demo {
-    private int a;
-    private int b;
-    private String c;
-    public demo(int a, int b){
-        this.a = a;
-        this.b = b;
-    }
-    public demo(int a, String c){
-        this.a = a;
-        this.c = c;
-    }
 
-    public int getA() {
-        return a;
-    }
-
-    public void setA(int a) {
-        this.a = a;
-    }
-
-    public int getB() {
-        return b;
-    }
-
-    public void setB(int b) {
-        this.b = b;
-    }
-
-    public String getC() {
-        return c;
-    }
-
-    public void setC(String c) {
-        this.c = c;
-    }
 }
