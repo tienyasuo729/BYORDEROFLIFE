2de449959bda42f3c06cbcb7c2843b02e608818e
tienyasuo729 <tienngqueson@gmail.com>
2022-12-02 04:11:51 +0700

    codegym

--- java/java/src/test/Point.java
+++ java/java/src/test/Point.java
@@ -4,4 +4,47 @@ public class Point {
     private float x;
     private float y;
 
+    public Point() {
+    }
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float[] getXY() {
+        float[] getXY = new float[2];
+        getXY[0] = getX();
+        getXY[1] = getY();
+        return getXY;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
