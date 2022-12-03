df22bfde2581294e9bdd01e379465eaefa78a2b9
tienyasuo729 <tienngqueson@gmail.com>
2022-12-03 23:31:02 +0700

    codegym

--- java/java/src/test/MoveablePoint.java
+++ java/java/src/test/MoveablePoint.java
@@ -1,4 +1,58 @@
 package test;
 
-public class MoveablePoint {
+public class MoveablePoint extends Point{
+    private float xSpeed;
+    private float ySpeed;
+
+    public MoveablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint() {
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed){
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed(){
+        float [] getSpeed = new float[2];
+        getSpeed[0] = this.xSpeed;
+        getSpeed[1] = this.ySpeed;
+        return getSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "MoveablePoint{" +
+                "x =" + getX() +
+                "y =" + getY() +
+                "xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
 }
