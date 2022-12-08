e8adbe415936d3d21a0226f8e46ab24e5ad292f5
tienyasuo729 <tienngqueson@gmail.com>
2022-12-09 01:25:07 +0700

    codegym

---
+++ java/java/src/DemoArrayList/aaaa.java
@@ -0,0 +1,15 @@
+package DemoArrayList;
+
+import java.util.ArrayList;
+
+public class aaaa {
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("1");
+        arr.add("2");
+        arr.add("3");
+        arr.add("4");
+        arr.add("5");
+        System.out.println(arr);
+    }
+}
