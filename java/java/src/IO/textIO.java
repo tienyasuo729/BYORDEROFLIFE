7b83b3e2d0fb1038fc494d7ca0518361367cc381
tienyasuo729 <tienngqueson@gmail.com>
2023-01-06 17:11:09 +0700

    codegym

--- java/java/src/IO/textIO.java
+++ java/java/src/IO/textIO.java
@@ -1,9 +1,20 @@
 package IO;
 
-import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class textIO {
-    public void IO(String filePath){
-        File file = new File(filePath);
+    public static void main(String[] args) throws IOException {
+        String name = "tiến";
+        try{
+            FileWriter fileWriter = new FileWriter("data.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(name);
+            bufferedWriter.close();
+            fileWriter.close();
+        }catch (Exception e){
+            System.out.println("error");
+        }
     }
 }
