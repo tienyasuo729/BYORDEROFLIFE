1e0170164801547122cc5a549b704bcd9f2776bb
tienyasuo729 <tienngqueson@gmail.com>
2023-02-15 19:42:15 +0700

    codegym

--- java/java/src/test/c.java
+++ java/java/src/test/c.java
@@ -1,37 +1,36 @@
 package test;
-
 import java.util.Scanner;
 
 public class c {
-    static Scanner scanner =new Scanner(System.in);
-
-    public static void main(String[] args) {
-        int n;
-        int i;
-        int k;
-        int m;
-        int check = 1;
-        System.out.println("Nhap n: ");
-        n = Integer.parseInt(scanner.nextLine());
-        System.out.print("\nSo nguyen to nho hon "+ n + " la: 1, ");
-//        for(k = 2; k <= n; k++){
-//		m = sqrt(k);
-            for(i = 2; i <= Math.sqrt(n); i++){
-                if(n % i == 0){
-                    System.out.print(" yes ");
-                    check = 0;
-                }
-            }
-// 	if(k % 2 != 0 && k % 3 != 0 && k % 5 != 0 && k % 7 != 0 || k == 2 || k == 3 || k == 5 || k == 7){
-// 		printf("%d, ",k);
-// 	}
-//            if(check == 1){
-//                System.out.println(k);
-//            }else{
-//                check = 1;
-//            }
-//        }
+    public static long giaithua(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * giaithua(n - 1);
     }
-
-
+    static Scanner scanner =new Scanner(System.in);
+    public static void main(String[] args) {
+//        int n;
+//        int phu;
+//        int n2 = 1;
+//        boolean check = true;
+//        do {
+//            System.out.print("Nhap n: ");
+//            n = Integer.parseInt(scanner.nextLine());
+//        }while (n < 0);
+//        phu = n;
+//        if (n == 0 || n == 1){
+//            System.out.print("!" + n + " = 1");
+//        }else {
+//            System.out.print("!" + n + " = 1");
+//            for (int i = 2; i <= phu; i++) {
+//                System.out.print(" x " + i);
+//                n *= (phu - i + 1);
+//            }
+//
+//
+//            System.out.print(" = " + n);
+//        }
+        System.out.println(giaithua(50));
+    }
 }
