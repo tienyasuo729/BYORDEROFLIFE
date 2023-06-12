34b7f6c30ef62dd43123fcefdf7f9ff347c5f838
tienyasuo729 <tienngqueson@gmail.com>
2023-06-12 16:51:54 +0700

    code

--- Module 4/Code/demo/src/main/java/com/Codegym/controller/Controller.java
+++ Module 4/Code/demo/src/main/java/com/Codegym/controller/Controller.java
@@ -1,4 +1,13 @@
 package com.Codegym.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+@RequestMapping("/hello")
 public class Controller {
+    @GetMapping("/show2")
+    public String show(){
+        return "demo";
+    }
 }
