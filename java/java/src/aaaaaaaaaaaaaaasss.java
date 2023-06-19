971badc3700474f0968fc1ba199b04160eaddc9d
tienyasuo729 <tienngqueson@gmail.com>
2023-06-19 11:33:59 +0700

    code

---
+++ java/java/src/aaaaaaaaaaaaaaasss.java
@@ -0,0 +1,6 @@
+public class aaaaaaaaaaaaaaasss {
+    public static void main(String[] args) {
+        int randomInt = (int) (Math.random() * 10);
+        System.out.println(randomInt);
+    }
+}
