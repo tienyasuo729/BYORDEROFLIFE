580aec086b0f2a34478ea504e63d90b18dfde28a
tienyasuo729 <tienngqueson@gmail.com>
2023-07-26 08:51:02 +0700

    code

--- java/java/src/testss/A.java
+++ java/java/src/testss/A.java
@@ -1,5 +1,14 @@
 package testss;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class A {
-    void foo() {System.out.printf("A");}
+
+    void f(String t) {System.out.printfln("String");}
+    void f(StringBuffer h) {System.out.printfln("StringBuffer");}
+    public static void main(String argv[]){
+        f("ABC");
+        System.out.printfln();
+    }
 }
