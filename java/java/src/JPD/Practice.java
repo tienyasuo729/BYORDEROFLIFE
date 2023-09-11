7da6c59ba66fc0f70353a5bc131deff981f2efb5
tienyasuo729 <tienngqueson@gmail.com>
2023-09-12 01:22:13 +0700

    code

--- java/java/src/JPD/Practice.java
+++ java/java/src/JPD/Practice.java
@@ -2,13 +2,14 @@ package JPD;
 
 public class Practice {
     public static void main(String[] args) {
-        String list1 = "a,i,u,e,o,ka,ki,ku,ke,ko,sa,shi,su,se,so,ta,chi,tsu,te,to,na,ni,nu,ne,no,ha,hi,fu,he,ho,ma,mi,mu,me,mo,ya,yu,yo,ra,ri,ru,re,ro,wa,o";
-        String translate = "あ,い,う,え,お";
+//        String list1 = "a,i,u,e,o,ka,ki,ku,ke,ko,sa,shi,su,se,so,ta,chi,tsu,te,to,na,ni,nu,ne,no,ha,hi,fu,he,ho,ma,mi,mu,me,mo,ya,yu,yo,ra,ri,ru,re,ro,wa,o";
+        String list1 = "a,i,u,e,o,ka,ki,ku,ke,ko,sa,shi,su,se,so";
+        String translate = "あ,い,う,え,お,";
         String [] a = list1.split(",");
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 20; i++) {
             System.out.print("\n" + i + ". ");
-            for (int j = 0; j < 5; j++) {
-                String random = a[(int) (Math.random()*44)];
+            for (int j = 0; j < 3; j++) {
+                String random = a[(int) (Math.random()*(a.length))];
                 System.out.print(random);
             }
 
