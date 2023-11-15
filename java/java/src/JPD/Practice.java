61ad73795cd6922cbe371ff223a32e2cf4ef514b
tienyasuo729 <tienngqueson@gmail.com>
2023-11-15 23:02:43 +0700

    code

--- java/java/src/JPD/Practice.java
+++ java/java/src/JPD/Practice.java
@@ -24,28 +24,29 @@ public class Practice {
 //        list1 = "かばん,ズボン,Tシャツ,とけい,～えん,いくら,じゃ,さかな,にく,ぎゅうにく,ぶたにく,やさい,いちご,りんご,りょうり,カレー,スープ,とんかつ,ハンバーグ,ごはん,ライス,ジュース,コーヒー,こうちゃ,（お）ちゃ,ビール";
 //        list1 = "sa,shi,su,se,so";
 //        list1 = "a,i,u,e,o,ka,ki,ku,ke,ko,sa,shi,su,se,so,ta,chi,tsu,te,to";
-        list1 = "ma,mi,mu,me,mo";
-        list1 = "na,ni,nu,ne,no,ha,hi,fu,he,ho,ma,mi,mu,me,mo";
 
-        int randomindex;
-        int[] tran = new int[4];
-        String[] a = list1.split(",");
-//        String[] b = translate.split(",");
-        for (int i = 1; i <= 27; i++) {
-            System.out.print("\n" + i + ". ");
-            for (int j = 0; j < tran.length; j++) {
-                randomindex = (int) (Math.random() * (a.length));
-                tran[j] = randomindex;
-                System.out.print(a[randomindex]);
-            }
-            System.out.println();
+//        list1 = "ra,ri,ru,re,ro";
+//        list1 = "a,i,u,e,o,ka,ki,ku,ke,ko,sa,shi,su,se,so,ta,chi,tsu,te,to,na,ni,nu,ne,no,ha,hi,fu,he,ho,ma,mi,mu,me,mo,ra,ri,ru,re,ro";
+
+//        int randomindex;
+//        int[] tran = new int[4];
+//        String[] a = list1.split(",");
+////        String[] b = translate.split(",");
+//        for (int i = 1; i <= 27; i++) {
+//            System.out.print("\n" + i + ". ");
+//            for (int j = 0; j < tran.length; j++) {
+//                randomindex = (int) (Math.random() * (a.length));
+//                tran[j] = randomindex;
+//                System.out.print(a[randomindex]);
+//            }
+//            System.out.println();
 
 //            System.out.println();
 //            for (int j = 0; j < tran.length; j++) {
 //                System.out.print(b[tran[j]]);
 //            }
 
-        }
+//        }
 
 //        String day [] = new String[31];
 //        String age [] = new String[100];
@@ -77,5 +78,21 @@ public class Practice {
 //            System.out.println((int) (Math.random() * (max - min + 1)) + min);
 //            System.out.println();
 //        }
+
+        list1 = "Tôi, tên, Đất Nước, Nhật Bản, Hàn Quốc, Trung Quốc, Mỹ, Ý, Úc, Nga, Thái, Trường cấp 3, Trường đại học, Trường Tiếng Nhật, Công việc, Học sinh, Thầy cô (xưng cô), Giáo viên, Nhân viên văn phòng, (tên cty) Nhân viên, Người (nước nào), Tiếng (nước nào), Xin chào (lần đầu gặp), Rất mong bạn giúp đỡ, Chính tôi mới là người (cần …), (ngập ngừng trc khi hỏi điều gì), Xin lỗi, Thế à (tiếp nhận thông tin), Ở đâu, Ngày sinh/sinh nhật, Brazil, Tháng (479) (trên lịch), Ngày (trên lịch), Tuổi (180), Lúc nào khi nào, Sở thích, Thể thao, Bóng đá, Bơi lội, Âm nhạc, Đọc sách, Du lịch, Món ăn, Cái gì, A! / Á!, Mỗi sáng";
+
+        int randomindex;
+        int[] tran = new int[1];
+        String[] a = list1.split(",");
+//        String[] b = translate.split(",");
+        for (int i = 1; i <= 27; i++) {
+            System.out.print("\n" + i + ". ");
+            for (int j = 0; j < tran.length; j++) {
+                randomindex = (int) (Math.random() * (a.length));
+                tran[j] = randomindex;
+                System.out.print(a[randomindex]);
+            }
+            System.out.println();
+        }
     }
 }
