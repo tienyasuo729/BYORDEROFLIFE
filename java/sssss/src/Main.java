3b8815f3d05d600bdade42e1b95375d376e9d806
tienyasuo729 <tienngqueson@gmail.com>
2024-03-19 16:01:03 +0700

    code

---
+++ java/sssss/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println(Validate.checkIntInPut("^([a-zA-ZÀ-ỹà-ỹ]+(?:\\s|$))*$","ad:","ab"));
+        }
+    }
+}
